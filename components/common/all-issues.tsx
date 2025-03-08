d4de2ee28a0a39f08643d77a00cb15d3927bacb7
Emmanuel KWENE <mystroken@gmail.com>
2025-03-08 07:24:40 +0100

    refactor: creates inner components to enhance code readability

--- components/common/all-issues.tsx
+++ components/common/all-issues.tsx
@@ -5,37 +5,48 @@ import { status } from '@/lib/mock-data/status';
 import { useIssuesStore } from '@/lib/store/issues-store';
 import { useSearchStore } from '@/lib/store/search-store';
 import { useViewStore } from '@/lib/store/view-store';
-import { SearchIssues } from './search-issues';
 import { cn } from '@/lib/utils';
+import { FC } from 'react';
+import { SearchIssues } from './search-issues';
 
 export default function AllIssues() {
-   const { issuesByStatus } = useIssuesStore();
    const { isSearchOpen, searchQuery } = useSearchStore();
    const { viewType } = useViewStore();
 
+   const isSearching = isSearchOpen && searchQuery.trim() !== '';
    const isViewTypeGrid = viewType === 'grid';
-   const issuesContainerClassName = isViewTypeGrid
-      ? 'flex h-full gap-3 px-2 py-2 min-w-max'
-      : undefined;
 
    return (
-      <div className={cn('w-full h-full', isViewTypeGrid ? 'overflow-x-auto' : '')}>
-         {isSearchOpen && searchQuery.trim() !== '' ? (
-            <div className="px-6 mb-6">
-               <SearchIssues />
-            </div>
+      <div className={cn('w-full h-full', isViewTypeGrid && 'overflow-x-auto')}>
+         {isSearching ? (
+            <SearchIssuesView />
          ) : (
-            <div className={issuesContainerClassName}>
-               {status.map((statusItem) => (
-                  <GroupIssues
-                     key={statusItem.id}
-                     status={statusItem}
-                     issues={issuesByStatus[statusItem.id] || []}
-                     count={issuesByStatus[statusItem.id]?.length || 0}
-                  />
-               ))}
-            </div>
+            <GroupIssuesListView isViewTypeGrid={isViewTypeGrid} />
          )}
       </div>
    );
 }
+
+const SearchIssuesView = () => (
+   <div className="px-6 mb-6">
+      <SearchIssues />
+   </div>
+);
+
+const GroupIssuesListView: FC<{
+   isViewTypeGrid: boolean;
+}> = ({ isViewTypeGrid = false }) => {
+   const { issuesByStatus } = useIssuesStore();
+   return (
+      <div className={cn(isViewTypeGrid && 'flex h-full gap-3 px-2 py-2 min-w-max')}>
+         {status.map((statusItem) => (
+            <GroupIssues
+               key={statusItem.id}
+               status={statusItem}
+               issues={issuesByStatus[statusItem.id] || []}
+               count={issuesByStatus[statusItem.id]?.length || 0}
+            />
+         ))}
+      </div>
+   );
+};
