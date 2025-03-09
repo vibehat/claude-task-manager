11a3636295388d72bce298a854da0bcff29e026d
ln-dev7 <leonelngoya@gmail.com>
2025-03-09 20:56:20 +0100

    adds lndev-ui section to app sidebar

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -13,6 +13,7 @@ import { Button } from '@/components/ui/button';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/components/ui/sidebar';
 import { teams } from '@/mock-data/teams';
 import Link from 'next/link';
+import { X } from 'lucide-react';
 
 const data = {
    inbox: [
@@ -47,6 +48,7 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+   const [open, setOpen] = React.useState(true);
    return (
       <Sidebar collapsible="offcanvas" {...props}>
          <SidebarHeader>
@@ -58,17 +60,50 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <NavTeams items={teams.filter((t) => t.joined)} />
          </SidebarContent>
          <SidebarFooter>
-            <div className="w-full flex items-center justify-between">
-               <HelpButton />
-               <Button size="icon" variant="secondary" asChild>
-                  <Link
-                     href="https://github.com/lndev-ui/circle"
-                     target="_blank"
-                     rel="noopener noreferrer"
-                  >
-                     <RiGithubLine className="size-4" />
-                  </Link>
-               </Button>
+            <div className="w-full flex flex-col gap-2">
+               {open && (
+                  <div className="group relative flex flex-col gap-2 rounded-lg border p-4 text-sm mt-6 w-full">
+                     <div
+                        className="absolute top-2.5 right-2 z-10 cursor-pointer"
+                        onClick={() => setOpen(!open)}
+                        role="button"
+                     >
+                        <X className="size-4" />
+                     </div>
+                     <div className="text-balance text-lg font-semibold leading-tight group-hover:underline">
+                        Fine components coded by lndev.
+                     </div>
+                     <div>
+                        A fun collection of small, well-coded components to streamline your
+                        development process.
+                     </div>
+                     <Link
+                        target="_blank"
+                        rel="noreferrer"
+                        className="absolute inset-0"
+                        href="https://ui.lndev.me"
+                     >
+                        <span className="sr-only">Deploy to Vercel</span>
+                     </Link>
+                     <Button size="sm" className="w-full">
+                        <Link href="https://ui.lndev.me" target="_blank" rel="noopener noreferrer">
+                           ui.lndev.me
+                        </Link>
+                     </Button>
+                  </div>
+               )}
+               <div className="w-full flex items-center justify-between">
+                  <HelpButton />
+                  <Button size="icon" variant="secondary" asChild>
+                     <Link
+                        href="https://github.com/lndev-ui/circle"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                     >
+                        <RiGithubLine className="size-4" />
+                     </Link>
+                  </Button>
+               </div>
             </div>
          </SidebarFooter>
       </Sidebar>
