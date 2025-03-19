38144c5a2242c2a7def517de203b1a3d64582a80
ln-dev7 <leonelngoya@gmail.com>
2025-03-19 10:05:53 +0100

    add teams page

--- app/[orgId]/teams/page.tsx
+++ app/[orgId]/teams/page.tsx
@@ -1,9 +1,11 @@
+import Teams from '@/components/common/teams/teams';
+import Header from '@/components/layout/headers/teams/header';
 import MainLayout from '@/components/layout/main-layout';
 
 export default function TeamsPage() {
    return (
-      <MainLayout header={<></>}>
-         <></>
+      <MainLayout header={<Header />}>
+         <Teams />
       </MainLayout>
    );
 }
