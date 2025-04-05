3b496a32b42e8ccb45c3848f6840f2953a16e030
ln-dev7 <leonelngoya@gmail.com>
2025-04-05 10:56:16 +0100

    Add settings page with features, integrations and guides components

--- app/[orgId]/settings/page.tsx
+++ app/[orgId]/settings/page.tsx
@@ -1,5 +1,11 @@
 import MainLayout from '@/components/layout/main-layout';
+import Settings from '@/components/common/settings/settings';
+import Header from '@/components/layout/headers/settings/header';
 
 export default function SettingsPage() {
-   return <MainLayout header={<></>}>Settings</MainLayout>;
+   return (
+      <MainLayout header={<Header />} headersNumber={1}>
+         <Settings />
+      </MainLayout>
+   );
 }
